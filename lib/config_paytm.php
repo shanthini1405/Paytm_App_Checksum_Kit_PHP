f1c101722ac85411312c4ca781eff87ad3f52851
shanthini1405 <53465799+shanthini1405@users.noreply.github.com>
2019-07-30 12:38:30 +0530

    Update config_paytm.php

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -1,4 +1,4 @@
 <?php
 //Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-define('PAYTM_MERCHANT_KEY', 'XXXXXXXXXXXXXXX'); 
+define('PAYTM_MERCHANT_KEY', '3gr4gIx7iqdiqyX#'); 
 ?>
